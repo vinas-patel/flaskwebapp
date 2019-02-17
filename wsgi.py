12b43d575ef445c4e03fa2462baada4a328da9e5
vinas-patel <vinas-patel>
2019-02-17 16:20:07 +0530

    app to application

--- wsgi.py
+++ wsgi.py
@@ -6,21 +6,21 @@ Created on Feb 16, 2019
 
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/test')
+@application.route('/test')
 def ajaxTest():
     return 'Hello there!'
 
-@app.route('/post', methods = ['POST'])
+@application.route('/post', methods = ['POST'])
 def talk():
     data = request.json['mg']
     msg = 'done';
     return jsonify({"msg": msg})
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    application.run(debug = True)
